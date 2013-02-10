6463a8371c40f25ba011336f3232678ff962142a
Guillaume Collic <gcollic@gmail.com>
2013-02-10 16:57:49 +0100

    Added powershell script to test Console2 colors (used for the screenshot)

---
+++ console2/writeColors.ps1
@@ -0,0 +1,26 @@
+﻿ $allColors = ("Black", 
+			   "DarkBlue",
+			   "DarkGreen",
+			   "DarkCyan",
+			   "DarkRed",
+			   "DarkMagenta",
+			   "DarkYellow",
+			   "Gray",
+			   "Darkgray",
+			   "Blue",
+			   "Green",
+			   "Cyan",
+			   "Red",
+			   "Magenta",
+			   "Yellow",
+			   "White")
+   
+foreach($fg in $allColors)
+{
+	foreach($bg in $allColors)
+	{
+		Write-Host " Test " -foreground $fg -background $bg -nonewline 
+	}
+	
+	Write-Host "" 
+}
